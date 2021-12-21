af20bd9c868d84ca6aa4a608350e21ff3acfd9df
Yeferson Marcano <yefelex10@gmail.com>
2021-12-21 11:06:39 -0300

    Update configDataTables.php

--- config/configDataTables.php
+++ config/configDataTables.php
@@ -6,12 +6,6 @@ $sql_details = array(
     'db' => 'bd_local',
     'host' => 'localhost',
    'port' => '5432'
-//$sql_details = array(
-//    'user' => 'sametsis',
-  //  'pass' => '+s4b1dur14+',
-  //  'db' => 'bd_local',
-  //  'host' => '192.168.0.91',
-  //  'port' => '5432'
 
 );
 
